822b1f7f447c7532e38efa0da160f085aaedb0b4
Artem Kulish <57228865+Artemkulish@users.noreply.github.com>
2020-05-06 19:14:21 +0300

    Update run.sh

--- identity-service/run.sh
+++ identity-service/run.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 
-bash SumoCollector.sh -q -varfile /home/sumo_credentials.txt -Vcollector.name=identity
+./SumoCollector.sh -q -varfile /home/sumo_credentials.txt -Vcollector.name=identity
 
 cd /home/ && java -jar *.jar
